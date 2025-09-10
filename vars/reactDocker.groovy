d260e62f96d7ab494826bd2a4b688acafe44c6e1
Sokroza Yeng <157787629+SokRoza@users.noreply.github.com>
2025-09-10 13:55:34 +0700

    fix error

--- vars/reactDocker.groovy
+++ vars/reactDocker.groovy
@@ -1,20 +1,15 @@
 def call(Map config = [:]) {
-    pipeline {
-        agent any
+    stage('Inject Dockerfile') {
+        steps {
+            script {
+                // Set variables here
+                def IMAGE_NAME = config.imageName ?: "react-app"
+                def REGISTRY   = config.registry ?: "dockerhub_username"
+                def PORT       = config.port ?: 80
 
-        environment {
-            IMAGE_NAME = config.imageName ?: "react-app"
-            REGISTRY   = config.registry ?: "dockerhub_username"
-            PORT       = config.port ?: 80
-        }
-
-        stages {
-            stage('Inject Dockerfile') {
-                steps {
-                    script {
-                        if (!fileExists('Dockerfile')) {
-                            echo "Dockerfile not found. Generating..."
-                            writeFile file: 'Dockerfile', text: """
+                if (!fileExists('Dockerfile')) {
+                    echo "Dockerfile not found. Generating..."
+                    writeFile file: 'Dockerfile', text: """
 FROM node:alpine AS builder
 WORKDIR /app
 COPY package*.json ./
@@ -27,19 +22,13 @@ COPY --from=builder /app/build /usr/share/nginx/html
 EXPOSE ${PORT}
 CMD ["nginx", "-g", "daemon off;"]
 """
-                        } else {
-                            echo "Dockerfile exists. Using existing."
-                        }
-                    }
+                } else {
+                    echo "Dockerfile exists. Using existing."
                 }
-            }
 
-            stage('Inject docker-compose.yml') {
-                steps {
-                    script {
-                        if (!fileExists('docker-compose.yml')) {
-                            echo "docker-compose.yml not found. Generating..."
-                            writeFile file: 'docker-compose.yml', text: """
+                if (!fileExists('docker-compose.yml')) {
+                    echo "docker-compose.yml not found. Generating..."
+                    writeFile file: 'docker-compose.yml', text: """
 version: '3.9'
 services:
   reactjs-srv:
@@ -48,10 +37,8 @@ services:
     ports:
       - "3000:${PORT}"
 """
-                        } else {
-                            echo "docker-compose.yml exists. Using existing."
-                        }
-                    }
+                } else {
+                    echo "docker-compose.yml exists. Using existing."
                 }
             }
         }
